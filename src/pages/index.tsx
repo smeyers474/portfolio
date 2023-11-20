24e64261676b3f0ab85d7665b53b24efbcc622f6
Sarah Meyers <sarmey@Sarahs-MacBook-Pro.local>
2023-11-20 13:16:35 -0800

    Add separate resume page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Link from "next/link";
 
 import { api } from "~/utils/api";
@@ -7,12 +6,6 @@ export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from San Diego" });
 
   return (
-    <>
-      <Head>
-        <title>Sarah Meyers</title>
-        <meta name="description" content="Portfolio created by Sarah Meyers" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#EBE3F1] to-[#CBC0D3]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
@@ -31,12 +24,12 @@ export default function Home() {
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-[#AFA4B7]/10 p-4 text-black hover:bg-[#AFA4B7]/20"
-              href="/resume.pdf"
-              target="_blank"
+              href="/resume"
+              target="_self"
             >
               <h3 className="text-2xl font-bold">Resume →</h3>
               <div className="text-lg">
-                PDF
+                Open Page
               </div>
             </Link>
           </div>
@@ -45,6 +38,5 @@ export default function Home() {
           </p>
         </div>
       </main>
-    </>
   );
 }
---
+++ src/pages/resume/index.tsx
@@ -0,0 +1,12 @@
+import dynamic from 'next/dynamic';
+
+/**
+ * Critical: prevents "TypeError: url.replace is not a function" error
+ */
+const Resume = dynamic(() => import('./Resume.tsx'), {
+  ssr: false,
+});
+
+export default function Page() {
+  return <Resume />;
+}
