19a92ec3241e3b98bf8931e97ce93d156a6fc9ce
Sarah Meyers <sarmey@Sarahs-MacBook-Pro.local>
2024-02-13 15:26:14 -0800

    Add description and fix photo ratio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,22 @@ import Image from "~/components/image/Image";
 export default function Home() {
   return (
     <Layout>
-      <div
+      <section
         className="container flex flex-row items-center justify-center gap-4 px-4 py-4"
         // className="container relative min-h-screen flex items-center"
       >
         <Image
           src="/images/headshot.jpg"
           alt="Picture of Sarah Meyers"
-          width="20vw"
-          height="50vh"
-          objectFit="contain"
+          width="17rem"
+          height="21.5rem"
         />
-        <p className="font-raleway text-2xl text-[#413e66]">
-          My name is Sarah Meyers
+        <p className="font-raleway text-lg text-[#413e66] w-[10rem]">
+          <h1 className="text-2xl pb-4">Hi, I'm Sarah.</h1>
+          San Diego based software developer who likes to spend her time
+          with hobbies like fashion and video games.
         </p>
-      </div>
+      </section>
     </Layout>
   );
 }
