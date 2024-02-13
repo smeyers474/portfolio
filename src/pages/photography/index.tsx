49f139a92c69a67d754c6d9b5920daad1c5bb9ef
Sarah Meyers <sarmey@Sarahs-MacBook-Pro.local>
2024-02-13 15:08:24 -0800

    Adjust photo grid

--- src/pages/photography/index.tsx
+++ src/pages/photography/index.tsx
@@ -7,36 +7,32 @@ const photoHeight = "25rem"
 export default function Page() {
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center gap-4 px-4 py-4 ">
-        <div className="flex flex-row items-center justify-center gap-4">
-          <Image
-            src="/images/plant.jpg"
-            alt="Picture of a plant"
-            width={photoWidth}
-            height={photoHeight}
-          />
-          <Image
-            src="/images/cityscape.jpg"
-            alt="Picture of a cityscape"
-            width={photoWidth}
-            height={photoHeight}
-          />
-        </div>
-        <div className="flex flex-row items-center justify-center gap-4">
-          <Image
-            src="/images/cityscapesunrise.jpg"
-            alt="Picture of a cityscape with sun shining"
-            width={photoWidth}
-            height={photoHeight}
-          />
-          <Image
-            src="/images/succulent.jpg"
-            alt="Picture of a succulent"
-            width={photoWidth}
-            height={photoHeight}
-          />
-        </div>
-      </div>
+      <section className="p-4 grid gap-4 grid-cols-1 xl:grid-cols-2 gap-4 auto-rows-[photoHeight]">
+        <Image
+          src="/images/plant.jpg"
+          alt="Picture of a plant"
+          width={photoWidth}
+          height={photoHeight}
+        />
+        <Image
+          src="/images/cityscape.jpg"
+          alt="Picture of a cityscape"
+          width={photoWidth}
+          height={photoHeight}
+        />
+        <Image
+          src="/images/cityscapesunrise.jpg"
+          alt="Picture of a cityscape with sun shining"
+          width={photoWidth}
+          height={photoHeight}
+        />
+        <Image
+          src="/images/succulent.jpg"
+          alt="Picture of a succulent"
+          width={photoWidth}
+          height={photoHeight}
+        />
+      </section>
     </Layout>
   );
 }
