d108a90f4066b31e0444b6c32fadd2f9ec53cc00
Sarah Meyers <sarmey@Sarahs-MacBook-Pro-2.local>
2024-03-01 16:44:16 -0800

    More format

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -5,29 +5,29 @@ export default function Page() {
     <Layout>
       <div className="py-5 font-raleway text-[#413e66]">
         <section className="p-10">
-        <div className="line"></div>
+          <div className="line"></div>
 
-            <h1 className="pb-5 text-2xl">Contact Me</h1>
-            <a
-              className="button text-md text-white"
-              href="mailto: sarmey47@gmail.com"
-            >
-              sarmey47@gmail.com
-            </a>
+          <h1 className="pb-5 text-2xl">Contact Me</h1>
+          <a
+            className="button text-md text-white"
+            href="mailto: sarmey47@gmail.com"
+          >
+            sarmey47@gmail.com
+          </a>
         </section>
         <section className="p-10">
-        <div className="line"></div>
+          <div className="line"></div>
 
-            <h1 className="pb-5 text-2xl">Social Media</h1>
-            <a
-              className="button text-md text-white"
-              href="https://www.linkedin.com/in/sarah-meyers-sm/"
-              target="_blank"
-            >
-              LinkedIn
-            </a>
+          <h1 className="pb-5 text-2xl">Social Media</h1>
+          <a
+            className="button text-md text-white"
+            href="https://www.linkedin.com/in/sarah-meyers-sm/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
         </section>
-        </div>
+      </div>
     </Layout>
   );
 }
